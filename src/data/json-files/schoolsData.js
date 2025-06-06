996e3ff9bf5700b37470d21bc5eb97c5b57f85f8
Francesco <fradeca01@gmail.com>
2025-06-06 22:55:56 +0200

    added modals to home page

---
+++ src/data/json-files/schoolsData.js
@@ -0,0 +1,259 @@
+import toppo from '../../assets/cards/3ddtw.svg'
+import iuss from '../../assets/cards/4iuss.svg'
+import santanna from '../../assets/cards/5santanna.svg'
+import bologna from '../../assets/cards/6bologna.svg'
+import roma from '../../assets/cards/7roma.svg'
+import lecce from '../../assets/cards/8lecce.svg'
+import sns from '../../assets/cards/9sns.svg'
+import galileiana from '../../assets/cards/10galileiana.svg'
+import circ from '../../assets/cards/11circ.svg'
+import catania from '../../assets/cards/12catania.svg'
+import torino from '../../assets/cards/13torino.svg'
+import macerata from '../../assets/cards/14macerata.svg'
+
+export const schoolsData = [
+    {
+        "id": "catania",
+        "label": "Catania",
+        "scuola": "Scuola Superiore di Catania",
+        "luogo": "Catania",
+        "image": catania,
+        "description": "La Scuola Superiore di Catania è stata fondata nel 1998 all’interno dell’Università degli Studi di Catania. Le prove del concorso di ammissione - rivolto agli studenti iscritti al I anno di un CdL triennale o magistrale a ciclo unico - si articolano in due Classi: la Classe delle Scienze umanistiche e sociali e la Classe delle Scienze sperimentali. Il superamento delle prove di concorso garantisce agli Allievi vitto e alloggio gratuiti presso Villa San Saverio, nonché sussidi finanziari per esperienze di mobilità. L’offerta formativa della Scuola si articola in una serie di corsi, differenziati negli anni: strumentali, interdisciplinari e di laboratorio (rivolti a studenti di primo e secondo anno); specialistici (affiancati all’attività di ricerca a partire dal terzo anno); corsi certificati di lingua straniera. A partire dal secondo anno, gli Allievi individuano un docente tutor, che coordina l’attività di ricerca attraverso progetti di formazione negli ambiti disciplinari designati dall’Allievo. La Scuola promuove periodi di soggiorno all’estero attraverso specifici contributi finanziari, nonché scambi presso le Università e istituzioni convenzionate. Il percorso formativo si conclude mediante la stesura di una tesi di diploma, valevole come Master accademico di II livello.",
+        "data": [
+            {
+                "nome": "Fabio",
+                "cognome": "Longo",
+                "email": "fabio.longo@catania.it"
+            },
+            {
+                "nome": "Silvia",
+                "cognome": "Testa",
+                "email": "silvia.testa@catania.it"
+            }
+        ]
+    },
+
+    {
+        "id": "pisa_normale",
+        "label": "Pisa (Normale)",
+        "scuola": "Scuola Normale Superiore di Pisa",
+        "luogo": "Pisa",
+        "image": sns,
+        "description": "",
+        "data": [
+            {
+                "nome": "Luca",
+                "cognome": "Verdi",
+                "email": "luca.verdi@pisa.it"
+            },
+            {
+                "nome": "Martina",
+                "cognome": "Neri",
+                "email": "martina.neri@pisa.it"
+            }
+        ]
+    },
+    {
+        "id": "pavia",
+        "label": "Pavia",
+        "scuola": "IUSS Scuola Universitaria Superiore di Pavia",
+        "luogo": "Pavia",
+        "image": iuss,
+        "description": "",
+        "data": [
+            {
+                "nome": "Francesco",
+                "cognome": "Russo",
+                "email": "francesco.russo@pavia.it"
+            },
+            {
+                "nome": "Sara",
+                "cognome": "Ferrari",
+                "email": "sara.ferrari@pavia.it"
+            }
+        ]
+    },
+    {
+        "id": "toppo",
+        "label": "Udine",
+        "scuola": "Scuola Superiore Universitaria Di Toppo Wassermann",
+        "description": "La Scuola Superiore Universitaria di Toppo Wassermann, attiva dal 2004, offre un percorso di formazione di eccellenza di carattere interdisciplinare, integrativo dei corsi di studio dell’Università di Udine. Essa si articola in due classi distinte: una classe Umanistica e una classe Scientifico-Economica (che comprende il corso di laurea in Medicina e Chirurgia). L’accesso è subordinato al superamento di un concorso di ammissione, il quale si rivolge ai futuri studenti del I e del IV anno. L’offerta formativa include corsi e seminari che mirano ad un apprendimento trasversale e partecipativo. La Scuola offre ai suoi studenti un approfondimento specialistico e interdisciplinare mediante laboratori, conferenze, ed eventi culturali. Da un punto di vista economico, all’esonero dal pagamento delle tasse universitarie si accompagna la possibilità di usufruire gratuitamente dei servizi mensa universitari e di un alloggio presso il palazzo settecentesco Toppo Wassermann. Gli allievi beneficiano inoltre dell’affiancamento a un tutor di alto profilo, di un bonus mobilità per studio o ricerca e di un fondo progetti. Il diploma conseguito al termine del percorso presso la Scuola è equiparato a un master di II livello.",
+        "luogo": "Udine",
+        "image": toppo,
+        "data": [
+            {
+                "nome": "Paolo",
+                "cognome": "Gallo",
+                "email": "paolo.gallo@camerino.it"
+            },
+            {
+                "nome": "Elena",
+                "cognome": "Romano",
+                "email": "elena.romano@camerino.it"
+            }
+        ]
+    },
+    {
+        "id": "bologna",
+        "label": "Bologna",
+        "scuola": "Collegio Superiore di Bologna",
+        "luogo": "Bologna",
+        "image": bologna,
+        "description": "Il Collegio Superiore, fondato nel 1998 presso l’Alma Mater Studiorum — Università di Bologna, offre percorsi formativi interdisciplinari avanzati a studentɜ selezionatɜ dell’Università. La didattica si articola in corsi integrati, tenuti da docenti di discipline diverse, e seminari. Lɜ collegiali sono ospitatɜ nella residenza Irnerio, nel centro di Bologna, possono partecipare a scambi con Scuole di eccellenza italiane ed estere e ricevono benefici economici (borsa di studio ed esenzione tasse) decisi annualmente. Ogni collegiale è inoltre seguitɜ da un tutor di alto profilo. Al termine dei 5 anni di Collegio e in seguito alla presentazione di un elaborato finale, lɜ collegiali ricevono un titolo equiparato a Master di II livello. Ogni anno vengono banditi posti per studentɜ di area umanistico-economica e di area scientifico-tecnica, in procinto di iniziare il primo anno di un corso di laurea triennale, magistrale o magistrale a ciclo unico.",
+        "data": [
+            {
+                "nome": "Simone",
+                "cognome": "Costa",
+                "email": "simone.costa@bologna.it"
+            },
+            {
+                "nome": "Chiara",
+                "cognome": "Marini",
+                "email": "chiara.marini@bologna.it"
+            }
+        ]
+    },
+    {
+        "id": "lecce",
+        "label": "Lecce",
+        "scuola": "Scuola superiore ISUFI di Lecce",
+        "luogo": "Lecce",
+        "image": lecce,
+        "description": "Fondata nel 1999, la Scuola Superiore ISUFI è un Istituto Superiore Universitario di Formazione Interdisciplinare incardinato nell’Università del Salento. Alla Scuola si accede tramite concorso per merito che viene bandito annualmente per le tre aree disciplinari (economico-giuridica, tecnico-scientifica ed umanistico-sociale). È possibile accedere al primo e secondo anno di Laurea Triennale/Magistrale a Ciclo Unico e al primo anno di Laurea Magistrale. Agli allievi selezionati, la Scuola offre l’alloggio presso il College, l’esenzione dalle tasse universitarie e un contributo mensile. L’offerta didattica è costituita da 2 corsi semestrali da 30 ore cadauno affiancati da diverse attività seminariali. Gli studenti inoltre hanno la possibilità di seguire i lettorati di lingue e devono svolgere un semestre di studi all’estero. Al termine del percorso, in seguito alla discussione di una tesi finale, la Scuola rilascia un titolo equiparato ad un Master di II livello.",
+        "data": [
+            {
+                "nome": "Davide",
+                "cognome": "Greco",
+                "email": "davide.greco@lecce.it"
+            },
+            {
+                "nome": "Valentina",
+                "cognome": "Fontana",
+                "email": "valentina.fontana@lecce.it"
+            }
+        ]
+    },
+    {
+        "id": "macerata",
+        "label": "Macerata",
+        "scuola": "Scuola di Studi Superiori 'Giacomo Leopardi' di Macerata",
+        "luogo": "Macerata",
+        "image": macerata,
+        "description": "Fondata nel 2008 presso l’Università degli Studi di Macerata, la “Scuola di Studi Superiori Giacomo Leopardi” è un istituto di alta formazione che si affianca ai corsi universitari e coltiva il talento di studentɜ selezionatɜ sulla base del solo merito. Ogni anno vengono banditi 10 posti per l’accesso al primo anno di un corso di laurea triennale o magistrale a ciclo unico e 2 posti per l’accesso al primo anno di un corso di laurea magistrale, ripartiti fra le due classi di Scienze Umanistiche e Scienze Sociali. L’offerta formativa si articola in seminari disciplinari e interdisciplinari tenuti da docenti di fama nazionale e internazionale, che consentono allɜ allievɜ di costruire un bagaglio di conoscenze ampio e trasversale avviandolɜ all’attività di ricerca. Lɜ allievɜ godono di una serie di benefici, fra i quali: esenzione dalle tasse universitarie, vitto gratuito presso le mense universitarie della città e alloggio gratuito presso la residenza Lombroso, situata all’interno del Collegio Universitario Matteo Ricci-Li Madou e immersa nel verde. Lɜ allievɜ hanno inoltre la possibilità di partecipare a scambi-mobilità con Scuole d’eccellenza italiane ed estere e sono seguitɜ da un tutor individuale. Al termine del percorso quinquennale, in seguito all’adempimento dei requisiti richiesti e alla discussione di una tesi finale, la Scuola rilascia un diploma equiparato a Master di II livello.",
+        "data": [
+            {
+                "nome": "Giorgio",
+                "cognome": "Sartori",
+                "email": "giorgio.sartori@macerata.it"
+            },
+            {
+                "nome": "Alessia",
+                "cognome": "Villa",
+                "email": "alessia.villa@macerata.it"
+            }
+        ]
+    },
+    {
+        "id": "torino",
+        "label": "Torino",
+        "scuola": "Scuola di Studi Superiori Ferdinando Rossi di Torino",
+        "luogo": "Torino",
+        "image": torino,
+        "description": "<p>La Scuola di Studi Superiori dell’Università degli Studi di Torino, di seguito indicata SSST, offre corsi che integrano il percorso formativo seguito dagli studenti iscritti ai Corsi di Studio dell’Ateneo. L’ammissione alla SSST avviene per concorso nazionale pubblico, esclusivamente sulla base di criteri di merito. I criteri di permanenza consistono nella frequenza di corsi (da 3 o 6 cfu) e sostenimento dei relativi esami per un totale di 15 cfu all’anno per 5 anni. Agli Studenti è richiesto il mantenimento di una media di almeno 27 trentesimi, sia dei corsi SSST che di quelli di Ateneo. Gli studenti che rispettano i suddetti criteri fruiscono di: </p> <ul> <li> ospitalità gratuita nelle residenze universitarie convenzionate</li>  <li>rimborso delle tasse universitarie</li>  <li>un contributo per lo studio. I corsi della Scuola affiancano quelli a cui sono iscritti gli studenti, integrandoli sia nei contenuti sia nelle forme di insegnamento. Sono dedicati a temi di forte impatto socio-politico, con particolare attenzione allo sviluppo sostenibile, e affrontati integrando scienze umane e della natura. Il corpo docente è composto sia da professori dell\'Università di Torino che da studiosi provenienti dall\'esterno - dall\'Italia e dall\'estero - in qualità di visiting professor.</li></ul>",
+        "data": [
+            {
+                "nome": "Matteo",
+                "cognome": "Conti",
+                "email": "matteo.conti@torino.it"
+            },
+            {
+                "nome": "Federica",
+                "cognome": "De Luca",
+                "email": "federica.deluca@torino.it"
+            }
+        ]
+    },
+    {
+        "id": "santanna",
+        "label": "Pisa (SantAnna)",
+        "scuola": "Scuola Superiore SantAnna di Pisa",
+        "luogo": "Pisa",
+        "image": santanna,
+        "description": "",
+        "data": [
+            {
+                "nome": "Andrea",
+                "cognome": "Barbieri",
+                "email": "andrea.barbieri@santanna.it"
+            },
+            {
+                "nome": "Laura",
+                "cognome": "Gentile",
+                "email": "laura.gentile@santanna.it"
+            }
+        ]
+    },
+   {
+        "id": "roma",
+        "label": "Roma",
+        "scuola": "Scuola di Studi Superiori della Sapienza",
+        "luogo": "Roma",
+        "image": roma,
+        "description": "<p>Istituita nel 2011 presso “La Sapienza - Università di Roma”, la SSAS offre un percorso formativo complementare di alta qualificazione per alliev3 meritevol3 iscritti ad un corso di laurea Sapienza. Gli alliev3 sono ospitati presso la Residenza “L. Serianni”, hanno diritto ad una borsa di studio e all’esenzione da tasse universitarie. L’offerta didattica si articola in:</p> <ul> <li>due corsi disciplinari semestrali a scelta ed afferenti alle diverse classi accademiche;</li> <li>un corso interdisciplinare su una tematica condivisa, studiata collettivamente con diverse - prospettive e metodi e i cui risultati vengono presentati durante il Simposio SSAS;</li><li>un progetto di ricerca annuale. Gli allievi, inoltre, sono seguiti nel proprio percorso accademico da un tutor scelto tra i fellows della Scuola e possono partecipare a scambi nazionali e internazionali con altre Scuole. Al termine di un percorso SSAS di 5 anni la Scuola eroga un titolo equiparato ad un Master di II livello.</li>s</ul>",
+        "data": [
+            {
+                "nome": "Riccardo",
+                "cognome": "Ferri",
+                "email": "riccardo.ferri@roma.it"
+            },
+            {
+                "nome": "Elisa",
+                "cognome": "Moretti",
+                "email": "elisa.moretti@roma.it"
+            }
+        ]
+    },
+
+    {
+        "id": "padova",
+        "label": "Padova",
+        "scuola": "Scuola Galileiana di Studi Superiori di Padova",
+        "luogo": "Padova",
+        "image": galileiana,
+        "description": "La Scuola Galileiana nasce nel 2004 ed è composta da student* che frequentano corsi di Laurea presso l’Università degli Studi di Padova. La Scuola è divisa in tre classi: Scienze Naturali (14 posti), Scienze Morali (8 posti) e Scienze Sociali (8 posti). Inoltre, da quest’anno sono aperte le iscrizioni per la magistrale. La vita della Scuola è scandita da corsi interni inerenti alle aree disciplinari delle tre classi e da eventi - spesso estesi al pubblico - come seminari e conferenze. Valore fondante della Scuola è la collegialità: all’interno della residenza, l* student* intessono legami di amicizia che resistono nel tempo. Coloro i quali concludano il proprio percorso quinquennale all’interno della Scuola ricevono un diploma, comparato a Master di secondo livello. L* student* hanno diritto ad un bonus mobilità, bonus acquisto libri e a vitto e alloggio gratuiti per tutta la durata del loro percorso; infine, sono seguiti da un* tutor cui possono rivolgersi per ogni eventualità.",
+        "data": [
+            {
+                "nome": "Stefano",
+                "cognome": "Rinaldi",
+                "email": "stefano.rinaldi@padova.it"
+            },
+            {
+                "nome": "Marta",
+                "cognome": "Serra",
+                "email": "marta.serra@padova.it"
+            }
+        ]
+    },
+
+ 
+    {
+        "id": "venezia",
+        "label": "Venezia",
+        "scuola": "Collegio Internazionale Ca’ Foscari di Venezia",
+        "luogo": "Venezia",
+        "image": circ,
+        "description": "Fondato nel 2012, il Collegio Internazionale Ca’ Foscari è la Scuola Superiore Universitaria dell’Ateneo veneziano. I collegiali sono ospitati presso la residenza Camplus nel centro di Venezia. Le vocazioni primarie del Collegio sono l’interdisciplinarietà e l’internazionalità. La lingua ufficiale del Collegio è l’inglese e l’offerta formativa rimarca il modello anglosassone grazie ai tre percorsi minor che caratterizzano il percorso formativo triennale. Il programma culturale viene completato anche dai cosiddetti Lab, ovvero corsi brevi di carattere trasversale, conferenze, workshops, convegni internazionali. Inoltre ogni studente è seguito da un professore del Board Accademico del Collegio. Il Collegio consente l’ingresso anche a studenti magistrali, i quali possono scegliere di frequentare un percorso in scienze umanistiche o in scienze sociali. Il percorso Collegiale prevede anche un periodo obbligatorio all’estero per ciclo di studi, in modo da ampliare quanto più possibile gli orizzonti dei propri allievi. Le selezioni avvengono indicativamente a inizio maggio per gli allievi triennali e a luglio per gli allievi magistrali. Il bando viene pubblicato un mese prima. Alla fine del percorso quinquennale, dopo aver adempiuto ai requisiti e aver discusso una tesi finale, il Collegio rilascia un diploma equiparato a Master di II livello.",
+        "data": [
+            {
+                "nome": "Alessandro",
+                "cognome": "Rossi",
+                "email": "alessandro.rossi@venezia.it"
+            },
+            {
+                "nome": "Giulia",
+                "cognome": "Bianchi",
+                "email": "giulia.bianchi@venezia.it"
+            }
+        ]
+    },
+]
