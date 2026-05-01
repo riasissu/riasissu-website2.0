347f8925157ae8bbeb06a7e3f22898fd5bd833a6
Alessandro Minisini <aleminipublic@gmail.com>
2026-05-01 15:27:20 +0200

    updated giunta e rappresentanti

--- src/data/json-files/giuntaData.js
+++ src/data/json-files/giuntaData.js
@@ -1,43 +1,43 @@
-import coordinatore_photo from '../../assets/giunta/maddalena_feltrin.webp'
-import tesoriere_photo from '../../assets/giunta/vittorio_magnaguagno.png'
-import vice_photo from '../../assets/giunta/giovanni_papi.png'
-import segretario_photo from '../../assets/giunta/giorgia_pazzi.png'
+import tesoriere_photo from '../../assets/giunta/paride_brogiolo.jpg'
+import coordinatore_photo from '../../assets/giunta/vittorio_magnaguagno.png'
+import vice_photo from '../../assets/giunta/lara_guglielmucci.jpg'
+import segretario_photo from '../../assets/giunta/niccolo.jpg'
 
 export const giuntaData = [
     {
-        id: "coordinatore",
-        nome: "Maddalena",
-        cognome: "Feltrin",
-        ruolo: "Coordinatrice Nazionale",
-        scuola: "Scuola Superiore di Toppo Wassermann di Udine",
-        description: "Maddalena Feltrin è allieva al III anno della \"Scuola Superiore di Toppo Wassermann\" dell'Università di Udine. Laureanda in lettere classiche, si interessa di letteratura, archeologia e didattica. Creativa e intraprendente, ama viaggiare, cucinare e disegnare; nel tempo libero si dedica ad attività di volontariato e nel futuro vorrebbe diventare una professoressa. Rappresentante per la sua Scuola da gennaio 2024 e referente di commissione Orientamento, da quest'anno ha deciso di impegnarsi ancor di più per la Rete al fine di organizzare e coordinare nuove concrete occasioni di scambio tra lɜ sociɜ: il sentimento di appartenenza a un'Associazione nazionale e il quotidiano dialogo tra allievɜ sono le opportunità promosse dalla Rete che più le stanno a cuore.",
-        picture: coordinatore_photo
+        id: "tesoriere",
+        nome: "Paride",
+        cognome: "Brogiolo",
+        ruolo: "Tesoriere",
+        scuola: "IUSS Scuola Universitaria Superiore di Pavia",
+        description: "Paride Brogiolo studia Scienze Filosofiche presso l'Università di Milano ed è allievo della classe di Scienze Umane presso la Scuola IUSS di Pavia. Appassionato di filosofia analitica, si occupa prevalentemente di logica modale e della sua applicazione nella formalizzazione di agenti epistemici, siano essi umani o artificiali. Nel tempo libero, si interessa di cinema e arte figurativa, con incursioni occasionali nel fumetto e nel gioco di società. Partito dalla rappresentanza universitaria e approdato poi a quella in RIASISSU, dal dicembre 2025 ricopre il ruolo di tesoriere nella Giunta Esecutiva della Rete.",
+        picture: tesoriere_photo
     },
     {
         id: "vice",
-        nome: "Giovanni",
-        cognome: "Papi",
-        scuola: "Scuola Superiore Sant'Anna di Pisa",
-        ruolo: "Coordinatore Nazionale Vicario",
-        description: "Giovanni Papi è un allievo del II anno della Scuola Superiore Sant’Anna di Pisa, dove frequenta il corso di laurea in Scienze Politiche. I suoi principali interessi accademici riguardano le relazioni internazionali, la storia e gli studi sulla sicurezza, con uno sguardo attento ai fenomeni globali e contemporanei. Vive nel collegio della Scuola, esperienza che considera centrale per la propria formazione, sia dal punto di vista umano che culturale. Giovanni crede profondamente nel valore della rete tra le Scuole di Studi Superiori Universitari e riconosce in RIASISSU uno strumento essenziale per promuovere il dialogo, la collaborazione e l’eccellenza condivisa tra lə Allievɜ. Per lui, contribuire alla crescita di questa comunità significa anche rafforzare il ruolo dell’università come luogo di apertura, responsabilità e innovazione.",
+        nome: "Lara",
+        cognome: "Guglielmucci",
+        scuola: "Scuola di Studi Superiori della Sapienza",
+        ruolo: "Coordinatrice Nazionale Vicario",
+        description: " Lara Guglielmucci è allieva al quarto anno della Classe Accademica di Studi Umanistici della Scuola Superiore di Studi Avanzati della Sapienza Università di Roma. È laureata in Arti e Scienze dello Spettacolo e in Filosofia e Intelligenza Artificiale presso lo stesso ateneo, con particolare interesse per i temi del bias culturale nell’AI generativa e per le sue implicazioni nella produzione audiovisiva, dal tema della creatività fino a quello della definizione del diritto d’autore. Ma giura di non essere una persona così seria. Più che le pergamene di laurea, incornicerebbe i cartelloni ironici con cui i suoi amici si sono presentati alla proclamazione e per cuocere la pasta la “risotta” direttamente nella padella del condimento, non per estro culinario ma per evitare di lavare anche la pentola dell’acqua. Dopo aver maturato esperienze come delegata, nel 2025 diventa rappresentante RIASISSU per la SSAS e, nello stesso anno, membro della Giunta Esecutiva in qualità di Coordinatrice Vicaria, con la speranza di poter contribuire concretamente a questa realtà e realizzare per i suoi Allievə Sociə iniziative di valore.",
         picture: vice_photo
     },
     {
         id: "segretario",
-        nome: "Giorgia",
-        cognome: "Pazzi",
-        ruolo: "Segretaria",
-        scuola: "Scuola di Studi Superiori \"G. Leopardi\" di Macerata",
-        description: "Giorgia Pazzi è un'allieva al V anno della Scuola di Studi Superiori \"G. Leopardi\" dell'Università di Macerata, e Rappresentante nella RIASISSU dal 2023. Laureata triennale in \"Lingue e culture straniere occidentali e orientali\" e alla fine del suo percorso magistrale in \"Lingue, culture e traduzione letteraria\", i suoi principali interessi ruotano intorno allo studio della letteratura - con una particolare predilezione per quella russa e per Dostoevskij -, ma spaziano anche dalla psicologia e criminologia all'ambito politico-economico, dalla sociologia alla filosofia. Si considera inoltre un'allieva molto affezionata della propria Scuola, e crede che lo spirito di Rete promosso da RIASISSU sia fondamentale ad uno sviluppo sempre maggiore della comunità di cui tuttɜ lɜ Allievɜ delle Scuole di Studi Superiori Universitari fanno parte.",
+        nome: "Niccolò",
+        cognome: "Gaspari",
+        ruolo: "Segretario",
+        scuola: "Scuola Superiore di Toppo Wassermann di Udine",
+        description: "Niccolò Gaspari è allievo della Scuola Superiore Di Toppo Wassermann di Udine e studente di Lettere all'Università di Udine. Rappresentante RIASISSU della Superiore friulana dal 2025, i suoi interessi accademici ruotano attorno all'indagine storica dei secoli di passaggio tra l'età di Antico Regime e la nuova epoca Contemporanea, in particolar modo riguardo all'area veneziana e friulana. Si interessa anche di storia della Chiesa e archeologia, disciplina che lo appassiona fin da piccolo e che lo ha ispirato nell'impostare il suo percorso di studi. E' inoltre allievo del Conservatorio di Udine, dove studia violino, strumento che lo accompagna fin dalla tenera età. Da dicembre 2025 entra a far parte della Giunta Esecutiva nel ruolo di Segretario.",
         picture: segretario_photo
     },
     {
-        id: "tesoriere",
+        id: "coordinatore",
         nome: "Vittorio",
-        ruolo: "Tesoriere",
+        ruolo: "Coordinatore",
         scuola: "Scuola Galileiana di Studi Superiori di Padova",
         cognome: "Magnaguagno",
-        description: "Vittorio Magnaguagno è un allievo della classe di Scienze Sociali della Scuola Galileiana di Studi Superiori ed è iscritto al corso di laurea in Scienze Psicologiche Sociali e del Lavoro presso l'Università degli Studi di Padova. I suoi interessi accademici ricadono principalmente negli ambiti della psicologia applicata, nello specifico verso le dimensioni legate alla psicologia dei gruppi e alla psicologia della decisione. Oltre a ciò si diverte a giocare con i lego, ascoltare musica classica e cucinare ottimi risotti. Dall’estate 2024 ricopre il ruolo di rappresentante Riasissu per la Scuola Galileiana e dal dicembre dello stesso anno è parte della Giunta Esecutiva in qualità di tesoriere, supportando la Coordinatrice Nazionale nell’amministrazione delle risorse della rete e nei principali adempimenti burocratici.",
-        picture: tesoriere_photo
+        description: "Vittorio Magnaguagno è un allievo della classe di Scienze Sociali della Scuola Galileiana di Studi Superiori ed è iscritto al corso di laurea magistrale in Psicologia Sociale del Lavoro e della Comunicazione presso l'Università degli Studi di Padova. I suoi interessi accademici ricadono principalmente negli ambiti della psicologia applicata, nello specifico verso le dimensioni legate alla psicologia dei gruppi e alla psicologia della decisione. Oltre a ciò si diverte a giocare con i lego, ascoltare musica classica e cucinare ottimi risotti. Dall'estate 2024 ricopre il ruolo di rappresentante Riasissu per la Scuola Galileiana e dal dicembre dello stesso anno è entrato a fare parte della Giunta Esecutiva in qualità di tesoriere. Dal dicembre 2025 ricopre invece il ruolo di Coordinatore Nazionale.",
+        picture: coordinatore_photo
     }
 ]
